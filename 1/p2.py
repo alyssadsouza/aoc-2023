c2b699aad4e6ac206d479d75790fe32fe5c0c4e5
alyssadsouza <alyssa.dsouza63@gmail.com>
2023-12-02 12:08:30 -0500

    switch to generator

--- 1/p2.py
+++ 1/p2.py
@@ -6,19 +6,24 @@ numbers = {"one": "1", "two": "2", "three": "3", "four": "4",
            "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
 
 
+def suffix(input: str):
+    """Returns a generator that yields the suffix of a string growing one char at a time"""
+    return (input[-i-1:] for i in range(len(input)))
+
+
 def getCalibrationValue(input: str) -> int:
     """Calculates calibration value for a string"""
     pattern = re.compile('\d|' + '|'.join(numbers.keys()))
 
     firstMatch = pattern.search(input).group(0)
     first = numbers.get(firstMatch, firstMatch)
-    # iterate in reverse in case of strings like 'twone'
-    reversed, i = "", -1
-    while not pattern.search(reversed):
-        reversed = input[i] + reversed
-        i -= 1
 
-    lastMatch = pattern.search(reversed).group(0)
+    # check from end of input in case of strings like 'twone'
+    inputSuffixGenerator, inputSuffix = suffix(input), ""
+    while not pattern.search(inputSuffix):
+        inputSuffix = next(inputSuffixGenerator)
+
+    lastMatch = pattern.search(inputSuffix).group(0)
     last = numbers.get(lastMatch, lastMatch)
 
     return int(first + last)
@@ -27,7 +32,7 @@ def getCalibrationValue(input: str) -> int:
 def sumCalibrations(inputFileName: str) -> int:
     """Reads in file input and sums over each line's calibration value"""
     with open(inputFileName, "r") as file:
-        calibrations = [getCalibrationValue(line) for line in file.readlines()]
+        calibrations = (getCalibrationValue(line) for line in file.readlines())
         return sum(calibrations)
 
 
