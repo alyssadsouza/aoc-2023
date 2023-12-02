bb42195048e1460f9b9d9fe1e4be87d833ecc784
alyssadsouza <alyssa.dsouza63@gmail.com>
2023-12-02 00:37:48 -0500

    day 1

---
+++ 1/p1.py
@@ -0,0 +1,31 @@
+import re
+
+############# Part 1 #############
+
+
+def getCalibrationValue(input: str) -> int:
+    """Calculates calibration value for a string"""
+    pattern = re.compile(r'\d')
+    first = pattern.search(input).group(0) # get first digit in string
+    last = pattern.search(input[::-1]).group(0) # get last digit by iterating in reverse
+    return int(first + last)
+
+
+def sumCalibrations(inputFileName: str) -> int:
+    """Reads in file input and sums over each line's calibration value"""
+    with open(inputFileName, "r") as file:
+        calibrations = [getCalibrationValue(line) for line in file.readlines()]
+        return sum(calibrations)
+
+
+# Test case
+assert (sumCalibrations("test-1.txt") == 142)
+
+# Other tests
+assert (getCalibrationValue("asdas1") == 11)
+assert (getCalibrationValue("1asdas1") == 11)
+assert (getCalibrationValue("dsa2sda3s") == 23)
+assert (getCalibrationValue("dsa22sdas") == 22)
+assert (getCalibrationValue("1ds3a223sda4s") == 14)
+
+print("Part 1:", sumCalibrations("input.txt"))
---
+++ 1/p2.py
@@ -0,0 +1,50 @@
+import re
+
+############# Part 2 #############
+
+numbers = {"one": "1", "two": "2", "three": "3", "four": "4",
+           "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+
+
+def getCalibrationValue(input: str) -> int:
+    """Calculates calibration value for a string"""
+    pattern = re.compile('\d|' + '|'.join(numbers.keys()))
+
+    firstMatch = pattern.search(input).group(0)
+    first = numbers.get(firstMatch, firstMatch)
+	# iterate in reverse in case of strings like 'twone'
+    reversed, i = "", -1
+    while not pattern.search(reversed):
+        reversed = input[i] + reversed
+        i -= 1
+
+    lastMatch = pattern.search(reversed).group(0)
+    last = numbers.get(lastMatch, lastMatch)
+
+    return int(first + last)
+
+
+def sumCalibrations(inputFileName: str) -> int:
+    """Reads in file input and sums over each line's calibration value"""
+    with open(inputFileName, "r") as file:
+        calibrations = [getCalibrationValue(line) for line in file.readlines()]
+        return sum(calibrations)
+
+
+# Test case
+assert (sumCalibrations("test-2.txt") == 281)
+
+# Other tests
+assert (getCalibrationValue("two1nine") == 29)
+assert (getCalibrationValue("1asdas1") == 11)
+assert (getCalibrationValue("eightwothree") == 83)
+assert (getCalibrationValue("abcone2threexyz") == 13)
+assert (getCalibrationValue("1ds3a223sda4s") == 14)
+assert (getCalibrationValue("xtwone3four") == 24)
+assert (getCalibrationValue("4nineeightseven2") == 42)
+assert (getCalibrationValue("zoneight234") == 14)
+assert (getCalibrationValue("7pqrstsixteen") == 76)
+assert (getCalibrationValue("oneight") == 18)
+assert (getCalibrationValue("twone") == 21)
+
+print("Part 2:", sumCalibrations("input.txt"))
